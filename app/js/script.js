77c24065af113cc764c7b2d75a85337ce8c468d4
Dmitry <dmitry.nsaa@gmail.com>
2017-12-18 17:46:53 +0300

    group+event

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,38 @@
 $(document).ready(function(){
+  /* слайдер на главной */
   $('.slider-header').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     dots: true
   });
-});
 
-
-$(document).ready(function(){
-    $('.slider-news').slick({
+/* слайдер с новостями и событиями */
+      $('.slider-news').slick({
       slidesToShow: 4,
       slidesToScroll: 1,
       arrows: true,
       dots: false,
     });
-  });
+    
+
+    /* слайдер с отзывами */
+      $('.feedback-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+    });
+});
+
+
+
+
+  // Dropdown navbar menu on hover
+
+
+$('.navbar-main .dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
+});
