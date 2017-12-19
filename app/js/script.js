986ed7c066d98276b015e0471666259267f9fcd0
Dmitry <dmitry.nsaa@gmail.com>
2017-12-19 18:02:28 +0300

    vnut

--- app/js/script.js
+++ app/js/script.js
@@ -36,3 +36,37 @@ $('.navbar-main .dropdown').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
 });
+
+
+
+//accordion menu
+jQuery(document).ready(function(){
+	var accordionsMenu = $('.cd-accordion-menu');
+
+	if( accordionsMenu.length > 0 ) {
+		
+		accordionsMenu.each(function(){
+			var accordion = $(this);
+			//detect change in the input[type="checkbox"] value
+			accordion.on('change', 'input[type="checkbox"]', function(){
+				var checkbox = $(this);
+				console.log(checkbox.prop('checked'));
+				( checkbox.prop('checked') ) ? checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300) : checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
+			});
+		});
+	}
+});
+
+
+
+$('#datepicker').datepicker({
+ 
+    leftArrow: '<i class="fa fa-long-arrow-left"></i>',
+    rightArrow: '<i class="fa fa-long-arrow-right"></i>'
+
+});
+$('#datepicker').on('changeDate', function() {
+    $('#my_hidden_input').val(
+        $('#datepicker').datepicker('getFormattedDate')
+    );
+});
